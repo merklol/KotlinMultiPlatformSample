62617db98b76e687d467004d5dfc7a517f58087e
Maxim Smolyakov <maxmerklol@gmail.com>
2022-12-27 19:42:18 +0800

    Remove a useless Resource annotation in Post.kt

--- shared/src/commonMain/kotlin/com/maximapps/kmmapp/Post.kt
+++ shared/src/commonMain/kotlin/com/maximapps/kmmapp/Post.kt
@@ -1,10 +1,8 @@
 package com.maximapps.kmmapp
 
-import io.ktor.resources.Resource
 import kotlinx.serialization.Serializable
 
 @Serializable
-@Resource("/posts")
 data class Post(
     val id: Int,
     val userId: Int,
